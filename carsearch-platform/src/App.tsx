9c5a3444053cc45e04eae23626a1d1be5243eb08
Bryan Hunt <b.buchan10@gmail.com>
2025-08-20 07:20:48 -0500

    functioning web page

--- carsearch-platform/src/App.tsx
+++ carsearch-platform/src/App.tsx
@@ -2,11 +2,18 @@ import Header from "./Header";
 import CarMakes from "./components/CarMakes";
 import CarModels from "./components/CarModels";
 import type { Make } from "./api";
+import { useState } from "react";
 
 function App() {
+	const [selectedMake, setSelectedMake] = useState<Make | null>(null);
+
 	return (
 		<div>
 			<Header />
+			<div style={{ display: "flex", gap: "2rem" }}>
+				<CarMakes onSelectMake={setSelectedMake} />
+				<CarModels make={selectedMake} />
+			</div>
 		</div>
 	);
 }
